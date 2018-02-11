771c5adc020bfd9bc7ebbf60ad89a100857e3802
ShrikantKY <shrikantyalaburgi@gmail.com>
2018-02-11 06:18:41 -0800

    Resume

---
+++ firstPorj/blog/views.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+
+
+
+def post_list(request):
+    return render(request, 'post_list.html', {})
+
+# Create your views here.
